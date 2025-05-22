2e081d6d38f37027ddc8cb2f1c11a3068dc9986b
Jonathan Cuotto <=>
2025-05-22 23:01:30 +0400

    excercises from section of two pointers

---
+++ two_pointers/3_Container With Most Water/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let max = 0;
+
+  while (left < right) {
+    h = Math.min(height[left], height[right]);
+    width = right - left;
+    area = h * width;
+    max = Math.max(max, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return max;
+};
